7073d38723dac108f8c48229a12eb6c5a300841a
bigbellyy <98248664+bigbellyy@users.noreply.github.com>
2025-10-31 01:28:41 -0700

    osr -> osz
    rewrite, now extracts osz files instead of osz.

--- src/data/generate_files.py
+++ src/data/generate_files.py
@@ -1,15 +1,15 @@
 import os
 import shutil
 import zipfile
-from util.osu_parser import OSUFile
 from pathlib import Path
 
 
 script_dir = Path(__file__).resolve().parent
 data_dir = (script_dir / "../../data").resolve()
-osr_dir = (data_dir / "osr").resolve()
+osz_dir = (data_dir / "osz").resolve()
 zip_dir = (data_dir / "zip").resolve()
 processed_dir = (data_dir / "processed").resolve()
+temp_dir = (data_dir / "temp").resolve()
 
 def _sanity_check():
     if not data_dir.is_relative_to(script_dir.parent.parent):
@@ -19,22 +19,25 @@ def _sanity_check():
 def load_osr():
     _sanity_check()
     
+    _processed_children = os.listdir(processed_dir)
+    file_id = len(_processed_children)
+    
     #Get all zip files
     zip_paths = zip_dir.rglob('*.zip')
     zips = set()
     for path in zip_paths:
         file_name = path.resolve().stem #File name, no extension.
         zips.add(file_name)
-            
+    
     #Get all osr files
-    osr_paths = osr_dir.rglob('*.osr')
-    for path in osr_paths:
+    osz_paths = osz_dir.rglob('*.osz')
+    for path in osz_paths:
         file_name = path.resolve().stem
-
+        
         #Continue on duplicates
         if file_name in zips:
             continue
-        
+
         #Clone file
         src = path.resolve()
         dst = zip_dir.resolve()
@@ -44,26 +47,35 @@ def load_osr():
         new_name = str(zip_dir / file_name) + ".zip"
         os.rename(copied_osr, new_name)
         
+        #create directory for osz
+        osz_dir_path = (processed_dir / str(file_id)).resolve()
+        if os.path.exists(osz_dir_path):
+            raise RuntimeError("Processed directory already exists.")
+        os.makedirs(osz_dir_path)
+        
         #extract zip
-        extraction_directory = (processed_dir / "temp").resolve()
-        
         with zipfile.ZipFile(new_name, 'r') as zip_ref:
-            zip_ref.extractall(extraction_directory)
+            zip_ref.extractall(osz_dir_path)
+        
+        audio_file = (osz_dir_path / "audio.mp3").resolve()
+        osu_file = str(osz_dir_path / file_name) + ".osu"     
+        
+        ####        clean up osz directories        ####
+        
+        #delete instruments, keep music file
+        
+        #mp3 pass
+        for path in osz_dir_path.iterdir():
+            if path.suffix != ".osu" and path.suffix != ".mp3": #audios are mp3
+                path.unlink()
             
-        audio_file = (extraction_directory / "audio.mp3").resolve()
-        osu_file = str(extraction_directory / file_name) + ".osu"     
+        #rename pass
+        for path in osz_dir_path.iterdir():
+            if path.suffix == ".mp3":
+                new_name = (path.parent / "audio.mp3") 
+                path.rename(new_name)
         
-        #rename files and move files
-        mp3_dir = (processed_dir / "mp3").resolve()
-        osu_dir = (processed_dir / "osu").resolve()
-        
-        #move to mp3 directory
-        new_name = str(mp3_dir / file_name) + ".mp3"
-        os.rename(audio_file, new_name)
-        
-        #move to osu directory
-        new_name = str(osu_dir / file_name) + ".osu"
-        os.rename(osu_file, new_name)
+        file_id+=1
 
 def load_dataset():
     pass
@@ -74,21 +86,13 @@ def _clean():
     for path in zip_paths:
         file_name = path.resolve()
         os.remove(file_name)
-    
-    mp3_dir = (processed_dir / "mp3")
-    osu_dir = (processed_dir / "osu")
-    
-    #Clear osu directory
-    osu_paths = osu_dir.rglob('*.osu')
-    for path in osu_paths:
-        file_name = path.resolve()
-        os.remove(file_name)
-
-    #Clear mp3 directory
-    mp3_paths = mp3_dir.rglob('*.mp3')
-    for path in mp3_paths:
-        file_name = path.resolve()
-        os.remove(file_name)
+        
+    #Clear processed
+    for path in processed_dir.iterdir():
+        if path.is_dir():
+            shutil.rmtree(path)
+        else:
+            path.unlink()
 
 
 if __name__ == "__main__":
@@ -97,5 +101,3 @@ if __name__ == "__main__":
     _clean()
     
     load_osr()
-    
-    print(str((data_dir / "osu") / "AAAA - Hoshizora no Kanransha.osu"))
